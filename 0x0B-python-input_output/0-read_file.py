0b053b85dc92402f2c16dbc08fdec2868ceea04d
Lacctoff <lasisitofunmi@gmail.com>
2023-11-07 15:39:01 -0800

    Adding files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that contains a function that reads from a file """
 
+
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
