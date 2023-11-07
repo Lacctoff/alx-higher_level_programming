f613309b37b99bfe06c8b4ee902ebbf8f3a558a2
Lacctoff <lasisitofunmi@gmail.com>
2023-11-07 15:37:52 -0800

    a funbctio that reads from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,17 +2,7 @@
 """ Module that contains a function that reads from a file """
 
 def read_file(filename=""):
-    """ Function that reads from a file
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-
-
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
